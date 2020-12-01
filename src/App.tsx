9d6c25f58b0afc8fd9e8965bfd4e56a8af9c340a
Matthew Sumner <msumner@hubspot.com>
2020-12-01 14:56:03 -0500

    Add router and link to DayOne

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { DayOne } from "./DayOne";
 
 export const App: React.FunctionComponent<Record<string, never>> = () => {
-  return <h1>Hello</h1>;
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/day_one">Day 1</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/day_one">
+            <DayOne />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
---
+++ src/DayOne.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const DayOne: React.FunctionComponent<Record<string, never>> = () => {
+  return <h1>HI</h1>;
+};
