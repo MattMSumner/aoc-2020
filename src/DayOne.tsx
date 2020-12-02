523f28ea0a28528001f0b795d6245d7688edbe3c
Matthew Sumner <msumner@hubspot.com>
2020-12-01 19:23:24 -0500

    Day One: Part One

--- src/DayOne.tsx
+++ src/DayOne.tsx
@@ -1,5 +1,30 @@
 import React from "react";
+import { expenseReport } from "./DayOne/expenseReport";
+
+function findPair(list: Array<number>): [number, number] {
+  const [head, ...rest] = list;
+  const pair = 2020 - head;
+
+  if (rest.includes(pair)) {
+    return [head, pair];
+  } else {
+    return findPair(rest);
+  }
+}
 
 export const DayOne: React.FunctionComponent<Record<string, never>> = () => {
-  return <h1>HI</h1>;
+  const [first, second] = findPair(expenseReport);
+  return (
+    <>
+      <h1>Day One; Expense Report</h1>
+
+      <p>
+        the pair is {first}, {second}
+      </p>
+
+      <p>
+        Multipled together: {first} x {second} = {first * second}
+      </p>
+    </>
+  );
 };
