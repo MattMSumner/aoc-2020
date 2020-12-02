9a4988db7af004965e462f1c706fb51e75a755e1
Matthew Sumner <msumner@hubspot.com>
2020-12-02 09:38:33 -0500

    Day Two; Part Two

--- src/DayTwo.tsx
+++ src/DayTwo.tsx
@@ -3,23 +3,39 @@ import { Password, passwords } from "./DayTwo/passwords";
 
 function isValid(password: Password): boolean {
   const [min, max, letter, passwordText] = password;
-
   const letterCount = passwordText
     .split("")
     .reduce(
       (count, character) => (character === letter ? count + 1 : count),
       0
     );
+
   return letterCount >= min && letterCount <= max;
+}
+
+function isTobogganValid(password: Password): boolean {
+  const [indexOne, indexTwo, letter, passwordText] = password;
+  const letters = passwordText.split("");
+
+  if (letters[indexOne - 1] === letter) {
+    return letters[indexTwo - 1] !== letter;
+  } else {
+    return letters[indexTwo - 1] === letter;
+  }
 }
 
 export const DayTwo: React.FunctionComponent<Record<string, never>> = () => {
   const validPasswordCount = passwords.filter(isValid).length;
+  const validTobogganPasswordCount = passwords.filter(isTobogganValid).length;
   return (
     <>
       <h1>Day Two; Passwords</h1>
 
       <p>There are {validPasswordCount} valid passwords</p>
+
+      <h2>Part Two</h2>
+
+      <p>There are {validTobogganPasswordCount} valid Toboggan passwords</p>
     </>
   );
 };
