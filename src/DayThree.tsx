59cbd8c47ee97bbf2c92f8d152f8f3e1956a266c
Matthew Sumner <msumner@hubspot.com>
2020-12-03 11:08:50 -0500

    Day Three; Part Two

--- src/DayThree.tsx
+++ src/DayThree.tsx
@@ -1,49 +1,122 @@
-import React from "react";
+import React, { useState } from "react";
 import { slopes } from "./DayThree/slopes";
 
-function slideDown(slopes: string[][]): [string[][], number] {
+function slideDown(
+  slopes: string[][],
+  velocity: [number, number]
+): [string[][], number] {
   let x = 0;
-  const yVelocity = 3;
+  const [xVelocity, yVelocity] = velocity;
   const repeat = Math.ceil(
-    (yVelocity * (slopes.length - 1)) / slopes[0].length
+    (xVelocity * (slopes.length - 1)) / slopes[0].length
   );
   let trees = 0;
 
   const map = slopes
     .map((row) => new Array(repeat).fill(row).flat())
-    .map((row) => {
-      if (row[x] === "#") {
+    .map((row, i) => {
+      if (i % yVelocity) {
+        console.log("skip");
+      } else if (row[x] === "#") {
         row[x] = "X";
         trees += 1;
+        x += xVelocity;
       } else {
         row[x] = "O";
+        x += xVelocity;
       }
-      x += 3;
       return row;
     });
 
   return [map, trees];
 }
 
+interface MapProps {
+  map: string[][];
+}
+
+const Map = ({ map }: MapProps) => {
+  const [showMap, setShowMap] = useState(false);
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          setShowMap(!showMap);
+        }}
+      >
+        {showMap ? "Hide" : "Show"} Map
+      </button>
+
+      {showMap && (
+        <div className="slope">
+          {map.map((row, i) => (
+            <div key={i}>
+              {row.map((element, j) => (
+                <div key={i + j} className="slope-element">
+                  {element}
+                </div>
+              ))}
+            </div>
+          ))}
+        </div>
+      )}
+    </>
+  );
+};
+
 export const DayThree: React.FunctionComponent<Record<string, never>> = () => {
-  const [map, trees] = slideDown(slopes);
+  const [amap, atrees] = slideDown(slopes, [1, 1]);
+  const [bmap, btrees] = slideDown(slopes, [3, 1]);
+  const [cmap, ctrees] = slideDown(slopes, [5, 1]);
+  const [dmap, dtrees] = slideDown(slopes, [7, 1]);
+  const [emap, etrees] = slideDown(slopes, [1, 2]);
   return (
     <>
       <h1>Day Three; Slopes</h1>
 
-      <div className="slope">
-        {map.map((row, i) => (
-          <div key={i}>
-            {row.map((element, j) => (
-              <div key={i + j} className="slope-element">
-                {element}
-              </div>
-            ))}
-          </div>
-        ))}
-      </div>
+      <Map map={bmap} />
 
-      <p>there were {trees} trees hit!</p>
+      <p>there were {btrees} trees hit!</p>
+
+      <h2>Part 2</h2>
+
+      <h3>Right 1, down 1.</h3>
+
+      <Map map={amap} />
+
+      <p>there were {atrees} trees hit!</p>
+
+      <h3>Right 3, down 1. (This is the slope you already checked.)</h3>
+
+      <Map map={bmap} />
+
+      <p>there were {btrees} trees hit!</p>
+
+      <h3>Right 5, down 1.</h3>
+
+      <Map map={cmap} />
+
+      <p>there were {ctrees} trees hit!</p>
+
+      <h3>Right 7, down 1.</h3>
+
+      <Map map={dmap} />
+
+      <p>there were {dtrees} trees hit!</p>
+
+      <h3>Right 1, down 2.</h3>
+
+      <Map map={emap} />
+
+      <p>there were {etrees} trees hit!</p>
+
+      <h2>Multiplied trees!</h2>
+
+      <p>
+        {atrees} x {btrees} x {ctrees} x {dtrees} x {etrees} ={" "}
+        {atrees * btrees * ctrees * dtrees * etrees}
+      </p>
     </>
   );
 };
