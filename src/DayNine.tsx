2102a79ecde3fb08fe3d650b4f1ab29c92b4fa26
Matthew Sumner <msumner@hubspot.com>
2020-12-09 07:59:52 -0500

    Day Nine; Part Two

--- src/DayNine.tsx
+++ src/DayNine.tsx
@@ -23,7 +23,6 @@ function findPair(list: Array<number>, sum: number): [number, number] | null {
 function findWeakness(stream: number[]): number {
   const preamble = stream.slice(0, preambleLength);
   const pair = findPair(preamble, stream[preambleLength]);
-  debugger;
 
   if (pair) {
     const newStream = stream.slice(1, stream.length);
@@ -33,12 +32,36 @@ function findWeakness(stream: number[]): number {
   }
 }
 
+function findSet(stream: number[], weakness: number): number[] {
+  let sum = 0;
+  let i = 0;
+  while (sum < weakness && i < stream.length) {
+    sum = sum + stream[i];
+    i++;
+  }
+  if (sum === weakness) {
+    return stream.slice(0, i);
+  } else {
+    const newStream = stream.slice(1, stream.length);
+    return findSet(newStream, weakness);
+  }
+}
+
 export const DayNine: React.FunctionComponent<Record<string, never>> = () => {
+  const stream = parseStream(xmasStream);
+  const weakness = findWeakness(stream);
+  const set = findSet(stream, weakness);
   return (
     <>
       <h1>Day Nine; Encoding Error</h1>
 
-      <p>{findWeakness(parseStream(xmasStream))}</p>
+      <p>the weakness is: {weakness}</p>
+
+      <p>the set is: {set.join(", ")}</p>
+
+      <p>the sum of the set is {Math.min(...set) + Math.max(...set)}</p>
     </>
   );
 };
+
+//15636471
