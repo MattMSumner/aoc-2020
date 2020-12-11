ffa34ed05a931066238c01cbe7984fdd6bb309e3
Matthew Sumner <msumner@hubspot.com>
2020-12-11 11:56:43 -0500

    Day Eleven; Part 2

--- src/DayEleven.tsx
+++ src/DayEleven.tsx
@@ -22,6 +22,8 @@ function parseLayout(layout: string): Layout {
   return { rows: rows.map(parseSpace) };
 }
 
+type CountNeighbours = (i: number, j: number, rows: Space[][]) => number;
+
 function countOccupiedNeighbours(
   i: number,
   j: number,
@@ -40,13 +42,54 @@ function countOccupiedNeighbours(
   return neighbours.length;
 }
 
-function tick(roomLayout: Layout): Layout {
+function visibleNeighbour(
+  i: number,
+  j: number,
+  direction: [number, number],
+  rows: Space[][]
+): Space | undefined {
+  const space = rows[i] && rows[i][j];
+  if (space === ".") {
+    return visibleNeighbour(
+      i + direction[0],
+      j + direction[1],
+      direction,
+      rows
+    );
+  } else {
+    return space;
+  }
+}
+
+function countVisibleOccupiedNeighbours(
+  i: number,
+  j: number,
+  rows: Space[][]
+): number {
+  const neighbours = [
+    visibleNeighbour(i - 1, j - 1, [-1, -1], rows),
+    visibleNeighbour(i - 1, j, [-1, 0], rows),
+    visibleNeighbour(i - 1, j + 1, [-1, 1], rows),
+    visibleNeighbour(i, j - 1, [0, -1], rows),
+    visibleNeighbour(i, j + 1, [0, 1], rows),
+    visibleNeighbour(i + 1, j - 1, [1, -1], rows),
+    visibleNeighbour(i + 1, j, [1, 0], rows),
+    visibleNeighbour(i + 1, j + 1, [1, 1], rows),
+  ].filter((space) => space === "#");
+  return neighbours.length;
+}
+
+function tick(
+  roomLayout: Layout,
+  tolerance: number,
+  countNeighbours: CountNeighbours
+): Layout {
   const rows = roomLayout.rows.map((row, i, rows) => {
     return row.map((space, j) => {
-      const occupiedNeighbours = countOccupiedNeighbours(i, j, rows);
+      const occupiedNeighbours = countNeighbours(i, j, rows);
       if (space === "L" && occupiedNeighbours === 0) {
         return "#";
-      } else if (space === "#" && occupiedNeighbours >= 4) {
+      } else if (space === "#" && occupiedNeighbours >= tolerance) {
         return "L";
       }
 
@@ -56,21 +99,42 @@ function tick(roomLayout: Layout): Layout {
   return { rows };
 }
 
-export const DayEleven: React.FunctionComponent<Record<string, never>> = () => {
-  const roomLayout = parseLayout(layout);
-  const [room, updateRoom] = useState(roomLayout);
+interface RoomProps {
+  room: Layout;
+}
+
+const Room: React.FunctionComponent<RoomProps> = ({ room }: RoomProps) => {
   const roomString = room.rows.map((row) => row.join("")).join("\n");
   const occupiedSeats = room.rows.flat().filter((space) => space === "#")
     .length;
+
+  return (
+    <>
+      <div className="room">{roomString}</div>
+
+      <p>There are {occupiedSeats} occupied seats</p>
+    </>
+  );
+};
+
+export const DayEleven: React.FunctionComponent<Record<string, never>> = () => {
+  const roomLayout = parseLayout(layout);
+  const [rooms, updateRoom] = useState([roomLayout, roomLayout]);
+  const onClick = () => {
+    updateRoom([
+      tick(rooms[0], 4, countOccupiedNeighbours),
+      tick(rooms[1], 5, countVisibleOccupiedNeighbours),
+    ]);
+  };
   return (
     <>
       <h1>Day Eleven; Seating System</h1>
 
-      <div className="room">{roomString}</div>
+      <Room room={rooms[0]} />
 
-      <p>There are {occupiedSeats} occupied seats</p>
+      <Room room={rooms[1]} />
 
-      <button onClick={() => updateRoom(tick(room))}>TICK</button>
+      <button onClick={onClick}>TICK</button>
     </>
   );
 };
