6623658ad53b99e4db94d54158bb0ab6ce7bf2e4
Matthew Sumner <msumner@hubspot.com>
2020-12-13 10:30:04 -0500

    Day Thirteen; Part Two

--- src/DayThirteen.tsx
+++ src/DayThirteen.tsx
@@ -23,14 +23,53 @@ function findClosestsBus(
   return results.find((x) => x[0] === minDeparture) || [0, 0];
 }
 
+function parseBussesChallenge(busses: string): [number, number][] {
+  const routes = busses.split("\n")[1];
+  return routes
+    .split(",")
+    .map<[string, number]>((x, i) => [x, i])
+    .filter((x) => x[0] !== "x")
+    .map((x) => [parseInt(x[0]), x[1]]);
+}
+
+function timestamp(
+  start: (i: number) => number,
+  list: [number, number][]
+): number {
+  const [[bus, offset], ...tail] = list;
+  let result;
+  if (offset === 0) {
+    result = bus;
+  }
+  let i = 0;
+  while (!result) {
+    i++;
+    if (start(i) >= bus && start(i) % bus === bus - (offset % bus)) {
+      result = start(i);
+    }
+  }
+  if (tail.length === 0) {
+    return result;
+  } else {
+    const newStart = (j: number) => start(i + bus * j);
+    return timestamp(newStart, tail);
+  }
+}
+
+function findTimestamp(routes: [number, number][]): number {
+  return timestamp((i) => i, routes);
+}
+
 export const DayThirteen: React.FunctionComponent<
   Record<string, never>
 > = () => {
   const [timestamp, routes] = parseBusses(busses);
   const [departure, bus] = findClosestsBus(timestamp, routes);
+  const routesWithPositions = parseBussesChallenge(busses);
+  const earliestDeparture = findTimestamp(routesWithPositions);
   return (
     <>
-      <h1>Day One; Expense Report</h1>
+      <h1>Day One; Shuttle Search</h1>
 
       <p>
         timestamp: {timestamp}, routes: {routes.join(", ")}
@@ -44,6 +83,12 @@ export const DayThirteen: React.FunctionComponent<
         wait time: {departure - timestamp}, anwser:{" "}
         {(departure - timestamp) * bus}
       </p>
+
+      <h2>Part Two</h2>
+
+      <p>routes: {routesWithPositions.flat().join(", ")}</p>
+
+      <p>earliest departure: {earliestDeparture}</p>
     </>
   );
 };
